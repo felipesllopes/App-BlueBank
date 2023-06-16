d85157a182a927c9e36c90fd55f4893fe9a8a9b8
felipeslopes <felipesl.lopes@hotmail.com>
2023-06-15 22:04:14 -0300

    atualizando projeto

--- src/pages/HistoricTransation/index.js
+++ src/pages/HistoricTransation/index.js
@@ -1,15 +1,43 @@
-import { View, Text, StyleSheet } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function HistoricTransation() {
     return (
-        <View>
-            <Text>Histórico de transação</Text>
+        <View style={styles.container}>
+
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={styles.tittle}>Histórico de transação</Text>
+
+                <TouchableOpacity>
+                    <FontAwesome name="calendar" size={30} color={'#000'} />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.box}>
+
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        backgroundColor: '#ADD8E6',
+    },
+    box: {
+        backgroundColor: '#FFF',
+        height: '90%',
+        width: '90%',
+        alignSelf: 'center',
+        borderRadius: 20,
+    },
+    tittle: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginVertical: 10,
+        marginRight: 20,
     },
 })
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,15 +1,129 @@
-import { View, Text, StyleSheet } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useContext } from "react";
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { AuthContext } from "../../context/auth";
 
 export default function Profile() {
+
+    const { user } = useContext(AuthContext);
+
     return (
-        <View>
-            <Text>Perfil</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.header}>
+                <Image style={styles.photo} source={require('../../img/user-photo.jpg')} />
+                <FontAwesome name="camera" size={20} color={"#FFF"} />
+                <TouchableOpacity activeOpacity={0.7}>
+                    <Text style={styles.textButtonPhoto}>Editar foto</Text>
+                </TouchableOpacity>
+            </View>
+
+            {user &&
+                <View style={styles.body}>
+                    <Text style={styles.tittle}>Meu perfil</Text>
+
+                    <Text style={styles.topic}>Meus dados</Text>
+
+                    <Text style={styles.text}>Nome</Text>
+                    <Text style={styles.information}>{user.name} {user.lastName}</Text>
+
+                    <Text style={styles.text}>Email</Text>
+                    <Text style={styles.information}>{user.email}</Text>
+
+                    <Text style={styles.text}>Telefone</Text>
+                    <Text style={styles.information}>{ }</Text>
+
+                    <Text style={styles.text}>CPF</Text>
+                    <Text style={styles.information}>{user.cpf}</Text>
+
+
+                    <Text style={styles.topic}>Endereço</Text>
+
+                    <Text style={styles.text}>Logradouro</Text>
+                    <Text style={styles.information}>{ }</Text>
+
+                    <Text style={styles.text}>Complemento</Text>
+                    <Text style={styles.information}>{ }</Text>
+
+                    <Text style={styles.text}>Bairro</Text>
+                    <Text style={styles.information}>{ }</Text>
+                </View>
+            }
+
+            <TouchableOpacity style={styles.buttonEdit} activeOpacity={0.7}>
+                <Text style={styles.textEdit}>Editar informações</Text>
+            </TouchableOpacity>
+
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        backgroundColor: '#ADD8E6'
+    },
+    header: {
+        backgroundColor: '#0000CD',
+        alignItems: 'center',
+        paddingVertical: 20,
+    },
+    photo: {
+        backgroundColor: '#FFF',
+        height: 100,
+        width: 100,
+        borderRadius: 80,
+        marginBottom: 20,
+    },
+    textButtonPhoto: {
+        color: '#FFF',
+        marginTop: 10,
+    },
+    body: {
+        padding: 10,
+    },
+    tittle: {
+        textAlign: 'center',
+        color: '#0000CD',
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    topic: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginVertical: 10,
+        color: '#0000CD',
+    },
+    text: {
+        fontSize: 19,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+    information: {
+        fontSize: 18,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        padding: 3,
+        paddingHorizontal: 10,
+        borderBottomWidth: 2,
+        borderColor: '#0000CD'
+    },
+    buttonEdit: {
+        alignItems: 'center',
+        backgroundColor: '#87CEEB',
+        marginVertical: 20,
+        paddingVertical: 4,
+        width: 220,
+        alignSelf: 'center',
+        borderRadius: 20,
+        justifyContent: 'center',
+        borderWidth: 2,
+        borderColor: '#0000CD',
+    },
+    textEdit: {
+        textAlign: 'center',
+        fontSize: 19,
+        fontWeight: 'bold',
+        color: '#0000CD',
     },
 })
